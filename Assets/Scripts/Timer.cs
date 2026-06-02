27610a410a46581adcecb951aa6ab1c3967f6b7c
William2057 <Williamarnell882@gmail.com>
2026-06-02 08:52:29 +0200

    .

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,44 +7,39 @@ public class Timer : MonoBehaviour
     public TextMeshProUGUI timerText;
     void Update()
     {
-        void Update()
+        if (GameManager.Instance == null)
+            return;
+        UpdateTimerUI();
+        if (GameManager.Instance.GameEnded)
+            return;
+        if (GameManager.Instance.Strikes >= 3)
         {
-            if (GameManager.Instance.GameEnded)
-                return;
-            if (GameManager.Instance.Strikes >= 3)
+            GameManager.Instance.GameEnded = true;
+            SceneManager.LoadScene(2);
+            return;
+        }
+        if (GameManager.Instance.TimerRunning)
+        {
+            GameManager.Instance.CurrentTime -= Time.deltaTime;
+
+            if (GameManager.Instance.CurrentTime <= 0)
             {
+                GameManager.Instance.CurrentTime = 0;
+                GameManager.Instance.TimerRunning = false;
                 GameManager.Instance.GameEnded = true;
-                SceneManager.LoadScene(3);
-                return;
-            }
-            if (GameManager.Instance.Strikes >= 3 && !GameManager.Instance.GameEnded)
-            {
-                GameManager.Instance.GameEnded = true;
-                SceneManager.LoadScene(3);
-                return;
-            }
-            if (GameManager.Instance.TimerRunning)
-            {
-                GameManager.Instance.CurrentTime -= Time.deltaTime;
 
-                if (GameManager.Instance.CurrentTime <= 0)
-                {
-                    GameManager.Instance.CurrentTime = 0;
-                    GameManager.Instance.TimerRunning = false;
-
-                    GameManager.Instance.GameEnded = true;
-
-                    CheckEndConditions();
-                    return;
-                }
+                CheckEndConditions();
             }
-            UpdateTimerUI();
         }
     }
     void UpdateTimerUI()
     {
+        if (timerText == null)
+            return;
+
         int minutes = Mathf.FloorToInt(GameManager.Instance.CurrentTime / 60);
         int seconds = Mathf.FloorToInt(GameManager.Instance.CurrentTime % 60);
+
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
     void CheckEndConditions()
@@ -57,6 +52,5 @@ public class Timer : MonoBehaviour
         {
             SceneManager.LoadScene(2);
         }
-
     }
 }
